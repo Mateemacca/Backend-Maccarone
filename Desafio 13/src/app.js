547daa9390a27adef05f7b04fc1f6f13ae73487c
Mate <mateobauti.maccarone@hotmail.com>
2024-04-16 00:52:47 -0300

    Desafio 13 - API Docs

---
+++ Desafio 13/src/app.js
@@ -0,0 +1,113 @@
+// -> Dependencias
+import express from "express";
+import handlebars from "express-handlebars";
+import swaggerJSDoc from "swagger-jsdoc";
+import { Server } from "socket.io";
+import session from "express-session";
+import FileStore from "session-file-store";
+import MongoStore from "connect-mongo";
+import mongoose from "mongoose";
+import passport from "passport";
+import swaggerUIExpress from "swagger-ui-express";
+
+// -> Configs
+import initializePassport from "./config/passport.config.js";
+import config from "./config/config.js";
+import { swaggerConfiguration } from "./utils/swagger-configuration.js";
+import { ErrorHandler } from "./middlewares/error.js";
+import { addLogger } from "./utils/logger.js";
+
+// -> Routes
+import viewsRouter from "./routes/views.routes.js";
+import sessionRoutes from "./routes/session.routes.js";
+import productsRouter from "./routes/products.routes.js";
+import cartRouter from "./routes/cart.routes.js";
+import testRoutes from "./routes/tester.routes.js";
+
+// -> Models
+import messageModel from "./dao/models/messages.model.js";
+
+const app = express();
+const fileStore = FileStore(session);
+const PORT = config.port;
+
+const specs = swaggerJSDoc(swaggerConfiguration);
+app.use("/apidocs", swaggerUIExpress.serve, swaggerUIExpress.setup(specs));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(addLogger);
+
+app.use(
+  session({
+    secret: config.secret,
+    store: MongoStore.create({
+      mongoUrl: config.mongoUrl,
+    }),
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+initializePassport();
+app.use(passport.initialize());
+app.use(passport.session());
+const hbs = handlebars.create({
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+  },
+  helpers: {
+    if_eq: function (a, b, opts) {
+      if (a === b) {
+        return opts.fn(this);
+      } else {
+        return opts.inverse(this);
+      }
+    },
+  },
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("views", "src/views");
+app.set("view engine", "handlebars");
+mongoose.connect(config.mongoUrl);
+
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartRouter);
+app.use("/", viewsRouter);
+app.use("/api/session", sessionRoutes);
+app.use("/api/test", testRoutes);
+app.use(ErrorHandler);
+
+const httpServer = app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
+const io = new Server(httpServer);
+const messages = [];
+
+io.on("connection", async (socket) => {
+  console.log("Usuario conectado");
+
+  try {
+    const storedMessages = await messageModel.find();
+    socket.emit("storedMessages", storedMessages);
+  } catch (error) {
+    console.error("Error al recuperar los mensajes almacenados:", error);
+  }
+
+  socket.on("message", async (data) => {
+    try {
+      await messageModel.create(data);
+      messages.push(data);
+      io.emit("messageLogs", [data]);
+    } catch (error) {
+      console.error("Error al almacenar el nuevo mensaje:", error);
+    }
+  });
+
+  socket.on("newUser", (user) => {
+    io.emit("newConnection", "Un nuevo usuario se conectó");
+    socket.broadcast.emit("notification", user);
+  });
+});
---
+++ Desafio 13/src/controllers/products.controller.js
@@ -0,0 +1,134 @@
+import ProductRepository from "../dao/repositories/product.repository.js";
+import productModel from "../dao/models/products.model.js";
+import { generateProduct } from "../utils/faker.js";
+export const productsService = new ProductRepository(productModel);
+export const getProducts = async (req, res) => {
+  try {
+    const { limit = 10, page = 1, query = "", sort = "" } = req.query;
+    const [code, value] = query.split(":");
+    const products = await productsService.getProducts(
+      limit,
+      page,
+      code,
+      value,
+      sort
+    );
+    res.send({ status: "success", ...products });
+  } catch (error) {
+    res.status(400).send({ error: "Error al obtener los productos" });
+    req.logger.error("Error al obtener productos");
+    console.log(error);
+  }
+};
+
+export const getProductById = async (req, res) => {
+  const { pid } = req.params;
+  try {
+    const product = await productsService.getProductById(pid);
+    if (product) {
+      req.logger.info("Producto por ID encontrado!");
+      res.send(product);
+    } else {
+      req.logger.error("Producto no encontrado");
+      res.status(404).send({ error: "Producto no encontrado" });
+    }
+  } catch (error) {
+    req.logger.error("Error al buscar el producto");
+    res.status(400).send({ error: "Error al obtener el producto" });
+  }
+};
+
+export const addProduct = async (req, res) => {
+  const { title, description, price, category } = req.body;
+
+  const userEmail = req.user ? req.user.email : null;
+
+  const owner = userEmail ? userEmail : "admin";
+
+  const userRole = req.user ? req.user.role : "admin";
+  if (userRole !== "premium" && userRole !== "admin") {
+    req.logger.error("Solo los usuarios premium pueden agregar productos.");
+    return res
+      .status(403)
+      .json({ error: "Solo los usuarios premium pueden agregar productos." });
+  }
+
+  try {
+    const newProduct = await productsService.addProduct({
+      title,
+      description,
+      price,
+      category,
+      owner,
+    });
+    res.status(201).json(newProduct);
+    req.logger.info("Producto agregado creado con éxito!");
+  } catch (error) {
+    req.logger.error("Error al crear el producto:", error);
+    res.status(400).send({ error: "Error al agregar el producto" });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { pid } = req.params;
+  try {
+    const updatedProduct = await productsService.updateProduct(pid, req.body);
+    if (updatedProduct) {
+      req.logger.info("Producto actualizado con exito");
+      res.send({ message: "Producto actualizado" });
+    } else {
+      req.logger.error("Producto no encontrado");
+      res.status(404).send({ error: "Producto no encontrado" });
+    }
+  } catch (error) {
+    req.logger.error("Error al actualizar el producto");
+    res.status(400).send({ error: "Error al actualizar el producto" });
+    console.error(error);
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { pid } = req.params;
+  const userRole = req.session.user.role;
+
+  try {
+    const product = await productsService.getProductById(pid);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    // verificar si es admin
+    if (userRole === "admin") {
+      await productsService.deleteProduct(pid);
+      req.logger.info("Producto eliminado con éxito por el administrador");
+      return res.status(200).json({ message: "Product deleted successfully" });
+    }
+
+    //  verificar si es owner del producto y si es premium
+    if (
+      userRole === "premium" &&
+      product.owner.toString() === req.session.user._id.toString()
+    ) {
+      await productsService.deleteProduct(pid);
+      req.logger.info("Producto eliminado con éxito por el usuario premium");
+      return res.status(200).json({ message: "Product deleted successfully" });
+    }
+
+    // Si el usuario no tiene los permisos necesarios
+    req.logger.error("El usuario no tiene permisos para eliminar el producto");
+    return res.status(403).json({ message: "horized" });
+  } catch (error) {
+    console.error("Error al eliminar el producto:", error);
+    req.logger.error("Error al eliminar el producto");
+    return res.status(400).send({ error: "Error al eliminar el producto" });
+  }
+};
+
+export const mockedProducts = async (req, res) => {
+  const users = [];
+  for (let i = 0; i < 100; i++) {
+    users.push(generateProduct());
+  }
+  res.send({ status: "success", payload: users });
+};
